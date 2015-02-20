e87e683c6668063050d76ec4a9c87d1f27a3ad8f
Emily Owaki <emily.owaki@gmail.com>
2015-02-20 12:35:08 -0600

    todo model setup

---
+++ js/models/todo.js
@@ -0,0 +1,15 @@
+var app = app || {};
+
+app.Todo = Backbone.Model.extend({
+  // Default attributes ensure that each todo created has `title` and `completed` keys.
+  defaults: {
+    title: '',
+    completed: false
+  },
+  //Toggle the completed state of this todo item
+  toggle: function() {
+    this.saves({
+      completed: !this.get('completed')
+    });
+  }
+});
